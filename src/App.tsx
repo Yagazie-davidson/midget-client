2abdf43a2e85fcae0663d02ed8c5691b119050bc
letam <davidsonletam@gmail.com>
2023-07-17 22:42:46 +0100

    change url display style

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,20 @@ function Form() {
         </label>
         <label htmlFor="short-url" className="block">
           <span className="block text-slate-700">Your Midget URL</span>
-          <input
+          {/* <input
             type="text"
             name="short-url"
             placeholder="eg. 94sQErMXW"
             className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-          />
+          /> */}
+          <div className="flex">
+            <div className="bg-slate-200 flex items-center px-3">
+              something.com/
+            </div>
+            <div className="text-slate-400 px-3 py-2 bg-white border shadow-sm border-slate-300 w-full sm:text-sm rounded-r-lg">
+              eg. 94sQErMXW
+            </div>
+          </div>
         </label>
         <motion.button
           aria-label="button to shorten URL"
