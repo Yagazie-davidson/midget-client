fe76751997caa18a59d2738fb051a505c7f49533
letam <davidsonletam@gmail.com>
2023-07-19 15:26:35 +0100

    change dummy url to deployed url

--- src/App.tsx
+++ src/App.tsx
@@ -54,9 +54,7 @@ function Form() {
             className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
           /> */}
           <div className="flex">
-            <div className="bg-slate-200 flex items-center px-3">
-              something.com/
-            </div>
+            <div className="bg-slate-200 flex items-center px-3">{baseUrl}</div>
             {loading && <p>Loading</p>}
             <div className="text-slate-400 px-3 py-2 bg-white border shadow-sm border-slate-300 w-full sm:text-sm rounded-r-lg">
               {urlId ? urlId : "eg. 94sQErMXW"}
