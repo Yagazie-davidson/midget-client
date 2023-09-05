5c4fe35fce0453ff8bf0930f8f029c26b46157bc
Letam Ugwuadu <77066722+Yagazie-davidson@users.noreply.github.com>
2023-09-05 17:12:23 +0100

    Revert "Pwa v"

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "midget-client",
-        short_name: "midget-client",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
-        lang: "en",
-        icons: [
-          {
-            src: "public/icons/manifest-icon-192.maskable.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "public/icons/manifest-icon-192.maskable.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
-          },
-          {
-            src: "public/icons/manifest-icon-512.maskable.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "public/icons/manifest-icon-512.maskable.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-        theme_color: "#ffffff",
-        scope: "/",
-      },
-
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-      },
-      devOptions: {
-        enabled: true,
-      },
-    }),
-    react(),
-  ],
-});
+  plugins: [react()],
+})
