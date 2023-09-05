8a71e7f6a0fb8175b1ed87ab09d1e4b6f2ecd110
letami0 <letam@izero.dev>
2023-09-05 16:58:14 +0100

    please work

---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "midget-client",
+        short_name: "midget-client",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        lang: "en",
+        icons: [
+          {
+            src: "public/icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+
+          {
+            src: "public/icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        scope: "/",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+    react(),
+  ],
+});
