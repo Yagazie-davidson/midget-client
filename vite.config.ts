31c62df95bdd82d94b27636ac20d9458dfb19098
letami0 <letam@izero.dev>
2023-09-05 13:14:58 +0100

    God abeg 2.5

--- vite.config.ts
+++ vite.config.ts
@@ -8,38 +8,6 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
-      manifest: {
-        name: "Midget",
-        short_name: "Midget",
-        description: "My Awesome App description",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "icons/manifest-icon-192.maskable.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "icons/manifest-icon-192.maskable.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
-          },
-          {
-            src: "icons/manifest-icon-512.maskable.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "icons/manifest-icon-512.maskable.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
