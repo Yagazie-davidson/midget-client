40771386d35a44f7d00d84f430c8ee2bf7b3d370
letami0 <letam@izero.dev>
2023-09-05 08:47:28 +0100

    God abeg v2.2

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,51 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+// import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 
-const manifestForPlugIn: Partial<VitePWAOptions> = {
-  registerType: "prompt",
-  includeAssets: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
-  manifest: {
-    name: "midget",
-    short_name: "midget",
-    description: "Url shortener",
-    icons: [
-      {
-        src: "icons/manifest-icon-192.maskable.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "icons/manifest-icon-192.maskable.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "maskable",
-      },
-      {
-        src: "icons/manifest-icon-512.maskable.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "icons/manifest-icon-512.maskable.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
-      },
-    ],
-    theme_color: "#171717",
-    background_color: "#f0e7db",
-    display: "standalone",
-    scope: "/",
-    start_url: "/",
-    orientation: "portrait",
-  },
-};
+// const manifestForPlugIn: Partial<VitePWAOptions> = {
+//   registerType: "prompt",
+//   includeAssets: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+//   manifest: {
+//     name: "midget",
+//     short_name: "midget",
+//     description: "Url shortener",
+//     icons: [
+//       {
+//         src: "icons/manifest-icon-192.maskable.png",
+//         sizes: "192x192",
+//         type: "image/png",
+//         purpose: "any",
+//       },
+//       {
+//         src: "icons/manifest-icon-192.maskable.png",
+//         sizes: "192x192",
+//         type: "image/png",
+//         purpose: "maskable",
+//       },
+//       {
+//         src: "icons/manifest-icon-512.maskable.png",
+//         sizes: "512x512",
+//         type: "image/png",
+//         purpose: "any",
+//       },
+//       {
+//         src: "icons/manifest-icon-512.maskable.png",
+//         sizes: "512x512",
+//         type: "image/png",
+//         purpose: "maskable",
+//       },
+//     ],
+//     theme_color: "#171717",
+//     background_color: "#f0e7db",
+//     display: "standalone",
+//     scope: "/",
+//     start_url: "/",
+//     orientation: "portrait",
+//   },
+// };
 
 export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPlugIn)],
+  plugins: [react()],
 });
