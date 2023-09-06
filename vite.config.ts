9485091cf1ccb286f88f2936e7779823d79ed41e
letami0 <letam@izero.dev>
2023-09-06 09:34:54 +0100

    pwa-latest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "midget-client",
+        short_name: "midget-client",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+          {
+            src: "public/iocns/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "public/iocns/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "public/iocns/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "public/iocns/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        background_color: "#ffffff",
+        lang: "en",
+        scope: "/",
+        theme_color: "#ffffff",
+      },
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+    react(),
+  ],
+});
