eeac48450397c75749157a300f630b0ff8d4ac92
letami0 <letam@izero.dev>
2023-09-06 10:00:32 +0100

    path

--- vite.config.ts
+++ vite.config.ts
@@ -16,25 +16,25 @@ export default defineConfig({
         display: "standalone",
         icons: [
           {
-            src: "public/iocns/manifest-icon-192.maskable.png",
+            src: "./public/iocns/manifest-icon-192.maskable.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "public/iocns/manifest-icon-192.maskable.png",
+            src: "./public/iocns/manifest-icon-192.maskable.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "maskable",
           },
           {
-            src: "public/iocns/manifest-icon-512.maskable.png",
+            src: "./public/iocns/manifest-icon-512.maskable.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "public/iocns/manifest-icon-512.maskable.png",
+            src: "./public/iocns/manifest-icon-512.maskable.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
